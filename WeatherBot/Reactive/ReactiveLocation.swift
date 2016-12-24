8d76b624c78683a33d9924d14ec0fc6b4fdc2ba8
Roman Roibu <romanroibu@gmail.com>
2016-12-24 21:39:45 +0100

    Add definition for ReactiveLocationService

---
+++ WeatherBot/Reactive/ReactiveLocation.swift
@@ -0,0 +1,40 @@
+//
+//  ReactiveLocation.swift
+//  WeatherBot
+//
+//  Created by Roman Roibu on 24/12/2016.
+//  Copyright © 2016 Roman Roibu. All rights reserved.
+//
+
+import CoreLocation
+import ReactiveSwift
+import Result
+
+public typealias Coordinate = (latitude: Double, longitude: Double)
+
+public enum LocationError: Error {
+    case unknown
+    case locationUnknown
+    case denied
+    case network
+    case deferredFailed
+    case deferredAccuracyTooLow
+    case deferredCanceled
+    case authorization(LocationAuthorizationError)
+}
+
+public enum LocationAuthorizationLevel {
+    case whenInUse
+    case always
+}
+
+public enum LocationAuthorizationError: Error {
+    case denied
+    case restricted
+}
+
+public protocol ReactiveLocationService {
+    static var singleCoordinate: SignalProducer<Coordinate, LocationError> { get }
+    static func requestAuthorization(desired: LocationAuthorizationLevel) -> SignalProducer<LocationAuthorizationLevel, LocationAuthorizationError>
+}
+
