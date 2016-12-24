fc7120ca0740250d8ae2c13f157da5ebc9b3e551
Roman Roibu <romanroibu@gmail.com>
2016-12-24 22:50:22 +0100

    Implement authorization request with AVCaptureDevice

--- WeatherBot/Reactive/ReactiveAudio.swift
+++ WeatherBot/Reactive/ReactiveAudio.swift
@@ -30,4 +30,43 @@ public protocol ReactiveInputAudioService {
 public final class ReactiveInputAudioCaptureSession: ReactiveInputAudioService {
     private static let mediaType = AVMediaTypeAudio
 
+    public static var requestAuthorization: SignalProducer<InputAudioAuthorizationLevel, InputAudioAuthorizationError> {
+        let authorizationChanges = SignalProducer<InputAudioAuthorizationLevel, InputAudioAuthorizationError> { observer, disposable in
+            AVCaptureDevice.requestAccess(forMediaType: self.mediaType) { granted in
+                if granted {
+                    observer.send(value: .authorized)
+                } else {
+                    observer.send(error: .denied)
+                }
+                observer.sendCompleted()
+            }
+        }
+
+        return SignalProducer<AVAuthorizationStatus, InputAudioAuthorizationError> { observer, disposable in
+                observer.send(value: AVCaptureDevice.authorizationStatus(forMediaType: self.mediaType))
+                observer.sendCompleted()
+            }
+            .flatMap(.latest) { status -> SignalProducer<InputAudioAuthorizationLevel, InputAudioAuthorizationError> in
+                if case .notDetermined = status {
+                    return authorizationChanges
+                } else {
+                    return self.authorizationLevel(from: status)
+                }
+        }
+    }
+
+    internal static func authorizationLevel(from status: AVAuthorizationStatus) -> SignalProducer<InputAudioAuthorizationLevel, InputAudioAuthorizationError> {
+        switch status {
+        case .authorized:
+            return SignalProducer(value: .authorized)
+        case .denied:
+            return SignalProducer(error: .denied)
+        case .restricted:
+            return SignalProducer(error: .restricted)
+        case .notDetermined:
+            assertionFailure("Should be determined by now")
+            return SignalProducer(error: .restricted)
+        }
+    }
+}
 }
