0d08b3fdf686509b00573e003caad3f61260a10b
Roman Roibu <romanroibu@gmail.com>
2016-12-24 21:30:03 +0100

    Add file for ReactiveSwift helper functions and extensions

---
+++ WeatherBot/Reactive/ReactiveSwift.swift
@@ -0,0 +1,12 @@
+//
+//  ReactiveSwift.swift
+//  WeatherBot
+//
+//  Created by Roman Roibu on 24/12/2016.
+//  Copyright © 2016 Roman Roibu. All rights reserved.
+//
+
+import Foundation
+import ReactiveSwift
+import Result
+
