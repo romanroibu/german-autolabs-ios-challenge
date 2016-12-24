b588041a3d1f794a8bbb5c075aca0e4dbaa750ee
Roman Roibu <romanroibu@gmail.com>
2016-12-24 21:43:05 +0100

    Add ReactiveLocationDelegate class and make it conform to CLLocationManagerDelegate
    It simply forwards all delegate calls to their respective signal outputs

--- WeatherBot/Reactive/ReactiveLocation.swift
+++ WeatherBot/Reactive/ReactiveLocation.swift
@@ -52,3 +52,35 @@ public protocol ReactiveLocationService {
     static func requestAuthorization(desired: LocationAuthorizationLevel) -> SignalProducer<LocationAuthorizationLevel, LocationAuthorizationError>
 }
 
+internal final class ReactiveLocationDelegate: NSObject, CLLocationManagerDelegate {
+
+    private let didChangeAuthorizationStatusPipe = Signal<CLAuthorizationStatus, NoError>.pipe()
+
+    private let didUpdateLocationsPipe = Signal<[CLLocation], NoError>.pipe()
+
+    private let didFailPipe = Signal<Error, NoError>.pipe()
+
+    internal var didChangeAuthorizationStatus: Signal<CLAuthorizationStatus, NoError> {
+        return self.didChangeAuthorizationStatusPipe.output
+    }
+
+    internal var didUpdateLocations: Signal<[CLLocation], NoError> {
+        return self.didUpdateLocationsPipe.output
+    }
+
+    internal var didFail: Signal<Error, NoError> {
+        return self.didFailPipe.output
+    }
+
+    internal func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        didChangeAuthorizationStatusPipe.input.send(value: status)
+    }
+
+    internal func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        didFailPipe.input.send(value: error)
+    }
+
+    internal func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        didUpdateLocationsPipe.input.send(value: locations)
+    }
+}
