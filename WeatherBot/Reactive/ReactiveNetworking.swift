8e1ba12c91187965cc1d2290e6c722951b8b9736
Roman Roibu <romanroibu@gmail.com>
2016-12-26 00:59:43 +0100

    Add a class that uses URLSession to conform to NetworkService

--- WeatherBot/Reactive/ReactiveNetworking.swift
+++ WeatherBot/Reactive/ReactiveNetworking.swift
@@ -13,3 +13,31 @@ import Result
 public protocol NetworkService {
     func load<T>(_ request: Request<T>) -> SignalProducer<T, AnyError>
 }
+
+public final class ReactiveURLSession: NetworkService {
+    private let session: URLSession
+
+    public init(session: URLSession = .shared) {
+        self.session = session
+    }
+
+    public init(configuration: URLSessionConfiguration) {
+        self.session = URLSession(configuration: configuration)
+    }
+
+    public func load<T>(_ request: Request<T>) -> SignalProducer<T, AnyError> {
+        return self.session
+            .reactive
+            .data(with: request.urlRequest)
+            .flatMap(.latest) { (data, response) -> SignalProducer<T, AnyError> in
+                //TODO: Return error signal if response code is not between 200 and 299
+                do {
+                    let value = try request.parse(data)
+                    return SignalProducer(value: value)
+                } catch {
+                    let anyError = AnyError(error)
+                    return SignalProducer(error: anyError)
+                }
+            }
+    }
+}
