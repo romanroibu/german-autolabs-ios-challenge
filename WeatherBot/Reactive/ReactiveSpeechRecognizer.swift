7fe70133d9ba5960db34bdddcefd3a207afd8129
Roman Roibu <romanroibu@gmail.com>
2016-12-25 17:40:26 +0100

    Define ReactiveSpeechRecognizer class and implement authorization request signal method

--- WeatherBot/Reactive/ReactiveSpeechRecognizer.swift
+++ WeatherBot/Reactive/ReactiveSpeechRecognizer.swift
@@ -30,3 +30,43 @@ public protocol SpeechRecognizerService {
     static func recognize<E: Error>(speech: Signal<CMSampleBuffer, E>, locale: Locale) -> SignalProducer<String, SpeechRecognizerError<E>>
     static var requestAuthorization: SignalProducer<SpeechRecognizerAuthorizationLevel, SpeechRecognizerAuthorizationError> { get }
 }
+
+public final class ReactiveSpeechRecognizer: SpeechRecognizerService {
+    public static var requestAuthorization: SignalProducer<SpeechRecognizerAuthorizationLevel, SpeechRecognizerAuthorizationError> {
+        let authorizationChanges = SignalProducer<SFSpeechRecognizerAuthorizationStatus, NoError> { observer, disposable in
+                SFSpeechRecognizer.requestAuthorization { status in
+                    observer.send(value: status)
+                }
+            }
+            .promoteErrors(SpeechRecognizerAuthorizationError.self)
+            .flatMap(.latest) { status -> SignalProducer<SpeechRecognizerAuthorizationLevel, SpeechRecognizerAuthorizationError> in
+                return self.authorizationLevel(from: status)
+            }
+
+        return SignalProducer<SFSpeechRecognizerAuthorizationStatus, SpeechRecognizerAuthorizationError> { observer, disposable in
+                observer.send(value: SFSpeechRecognizer.authorizationStatus())
+                observer.sendCompleted()
+            }
+            .flatMap(.latest) { status -> SignalProducer<SpeechRecognizerAuthorizationLevel, SpeechRecognizerAuthorizationError> in
+                if case .notDetermined = status {
+                    return authorizationChanges
+                } else {
+                    return self.authorizationLevel(from: status)
+                }
+            }
+    }
+
+    internal static func authorizationLevel(from status: SFSpeechRecognizerAuthorizationStatus) -> SignalProducer<SpeechRecognizerAuthorizationLevel, SpeechRecognizerAuthorizationError> {
+        switch status {
+        case .authorized:
+            return SignalProducer(value: .authorized)
+        case .denied:
+            return SignalProducer(error: .denied)
+        case .restricted:
+            return SignalProducer(error: .restricted)
+        case .notDetermined:
+            assertionFailure("Should be determined by now")
+            return SignalProducer(error: .restricted)
+        }
+    }
+}
