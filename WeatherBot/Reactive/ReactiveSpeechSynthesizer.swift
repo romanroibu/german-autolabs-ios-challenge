13db982d35b60d09d81d6db7ec1fd14877112bd6
Roman Roibu <romanroibu@gmail.com>
2016-12-25 23:25:04 +0100

    Add reactive AVSpeechSynthesizerDelegate class that forwards all delegate calls to its signals

--- WeatherBot/Reactive/ReactiveSpeechSynthesizer.swift
+++ WeatherBot/Reactive/ReactiveSpeechSynthesizer.swift
@@ -15,3 +15,23 @@ public protocol SpeechSynthesizerService {
     static func speak(text: String, language: Language) -> SignalProducer<NSRange, NoError>
 }
 
+internal final class ReactiveSpeechSynthesizerDelegate: NSObject, AVSpeechSynthesizerDelegate {
+
+    private let willSpeakRangeOfSpeechPipe = Signal<NSRange, NoError>.pipe()
+
+    var willSpeakRangeOfSpeech: Signal<NSRange, NoError> {
+        return self.willSpeakRangeOfSpeechPipe.output
+    }
+
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didCancel utterance: AVSpeechUtterance) {
+        self.willSpeakRangeOfSpeechPipe.input.sendInterrupted()
+    }
+
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) {
+        self.willSpeakRangeOfSpeechPipe.input.sendCompleted()
+    }
+
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, willSpeakRangeOfSpeechString characterRange: NSRange, utterance: AVSpeechUtterance) {
+        self.willSpeakRangeOfSpeechPipe.input.send(value: characterRange)
+    }
+}
