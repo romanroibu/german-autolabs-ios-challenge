43a1789fb8c390ff6396d4731e9b3796cd85b382
Roman Roibu <romanroibu@gmail.com>
2016-12-28 00:22:05 +0100

    Add table view footer (record button wrapper) lazy property

--- WeatherBot/ViewController.swift
+++ WeatherBot/ViewController.swift
@@ -65,6 +65,44 @@ class ViewController: UITableViewController {
         button.addTarget(self, action: #selector(self.listenAction(_:)), for: .touchUpInside)
         return button
     }()
+
+    lazy var tableFooter: UIView = {
+        let footer = UIView(frame: self.listenButton.bounds)
+        self.listenButton.translatesAutoresizingMaskIntoConstraints = false
+
+        footer.addSubview(self.listenButton)
+
+        let constraints: [NSLayoutConstraint] = [
+            NSLayoutConstraint(
+                item: self.listenButton,
+                attribute: .centerX,
+                relatedBy: .equal,
+                toItem: footer,
+                attribute: .centerX,
+                multiplier: 1,
+                constant: 0
+            ),
+            NSLayoutConstraint(
+                item: self.listenButton,
+                attribute: .height,
+                relatedBy: .equal,
+                toItem: self.listenButton,
+                attribute: .width,
+                multiplier: 1,
+                constant: 0
+            ),
+        ] + NSLayoutConstraint.constraints(
+            withVisualFormat: "V:|[button]|",
+            options: [],
+            metrics: nil,
+            views: ["button": self.listenButton]
+        )
+
+        NSLayoutConstraint.activate(constraints)
+
+        return footer
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
